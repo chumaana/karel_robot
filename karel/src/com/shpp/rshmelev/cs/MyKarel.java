a8872efed577e439acb49da3fe2d182c332d73b7
Anastasiia Chumak <chumaana@fit.cvut.cz>
2023-08-24 23:37:59 +0200

    added class MyKarel

---
+++ karel/src/com/shpp/rshmelev/cs/MyKarel.java
@@ -0,0 +1,13 @@
+package com.shpp.rshmelev.cs;
+
+import com.shpp.karel.KarelTheRobot;
+
+public class MyKarel extends KarelTheRobot {
+
+    /* це коментар :) */
+    public void run() throws Exception {
+        move();
+        pause();
+        turnLeft();
+    }
+}
