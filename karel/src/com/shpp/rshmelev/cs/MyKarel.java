3bb41712a33e04300e059239f6b08b6fb1eae5a8
Anastasiia Chumak <chumaana@fit.cvut.cz>
2023-08-24 23:55:49 +0200

    add some code to MyKarel

--- karel/src/com/shpp/rshmelev/cs/MyKarel.java
+++ karel/src/com/shpp/rshmelev/cs/MyKarel.java
@@ -7,22 +7,49 @@ public class MyKarel extends KarelTheRobot{
 //  this is method where Karel pics beeper
 //    and moves it to the destination
 
+////start
+//    public void run() throws Exception {
+////    find beeper and pick it
+//        move();
+//        move();
+//        move();
+//        move();
+//       pickBeeper();
+//
+////       put beeper to final destination
+//       turnLeft();
+//       move();
+//       move();
+//       turnLeft();
+//       move();
+//       move();
+//       putBeeper();
+//    }
 
-    public void run() throws Exception {
-//    find beeper and pick it
-        move();
-        move();
-        move();
-        move();
-       pickBeeper();
+//start2
+public void run() throws Exception {
+//      go to second row
+    move();
+   turnLeft();
+   move();
+
+    //    find beeper and pick it
+    turnRight();
+    move();
+    pickBeeper();
 
 //       put beeper to final destination
-       turnLeft();
-       move();
-       move();
-       turnLeft();
-       move();
-       move();
-       putBeeper();
-    }
+    turnLeft();
+    move();
+    turnRight();
+    move();
+    move();
+    putBeeper();
+}
+
+private void turnRight() throws Exception {
+    turnLeft();
+    turnLeft();
+    turnLeft();
+}
 }
